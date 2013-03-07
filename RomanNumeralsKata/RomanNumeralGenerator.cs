545e04cf6e97acaa98869e877ba7f38bdb448a0f
Ryan Gough <ryan.gough@gmail.com>
2013-03-07 23:46:57 +0000

    Refactoring to remove duplication in generator
    There was clear duplication in the generator class with checking for
    values and returning strings. There seemed to be a good case for using a
    dictionary to map values to roman numeral strings. I left 0 out of the
    map to show that it is a special (degenerate) case.

--- RomanNumeralsKata/RomanNumeralGenerator.cs
+++ RomanNumeralsKata/RomanNumeralGenerator.cs
@@ -1,20 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace RomanNumeralsKata
 {
     public class RomanNumeralGenerator
     {
+        private IDictionary<int, string> _ValueToNumeralMap = new Dictionary<int, string>()
+        {
+            { 1, "I"},
+            { 5, "V"},
+        };
+
         public string Generate(int value)
         {
             if (value == 0)
             {
                 return String.Empty;
             }
-            if (value == 1)
-            {
-                return "I";
-            }
-            return "V";
+            return _ValueToNumeralMap[value];
         }
     }
 }
