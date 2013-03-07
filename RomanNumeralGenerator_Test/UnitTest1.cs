3a41c3f3f6b50b8bb6652d7f6ed7fa595e1cafa0
Ryan Gough <ryan.gough@gmail.com>
2013-03-07 23:20:06 +0000

    First failing test (degenerate case)
    I've chosen to implement the degenerate case of 0 returning empty string
    as the first test as it seemed the simplest case to start with.

---
+++ RomanNumeralGenerator_Test/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RomanNumeralsKata;
+
+namespace RomanNumeralGenerator_Test
+{
+    [TestClass]
+    public class RomanNumeralGeneratorTestSuite
+    {
+        [TestMethod]
+        public void Generate_Given_0_Returns_EmptyString()
+        {
+            RomanNumeralGenerator generator = new RomanNumeralGenerator();
+            string expectedResult = string.Empty;
+            string actualResult = generator.Generate(0);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
---
+++ RomanNumeralsKata/RomanNumeralGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RomanNumeralsKata
+{
+    public class RomanNumeralGenerator
+    {
+        public string Generate(int value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
